cecda89775eaf2cf9449ad84883538ba01d00cd6
Bruno Vasconcelos <brunohgv2@gmail.com>
2020-09-27 13:38:12 -0300

    Update readme and procedure call

---
+++ README.md
@@ -0,0 +1,17 @@
+# GRPC Test
+
+Simple test using grpc to print a hello world
+
+## How to use
+
+### Run the server
+```
+go run server/main.go
+```
+
+### Run the client
+```
+go run server/main.go [YOUR_NAME]
+```
+
+It should run a procedure call and return "Hello \[YOUR_NAME\]"
--- client/main.go
+++ client/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/brunohgv/grpc_test/protobuffer"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	arguments := os.Args[1:]
 	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not stablish connection:\n %v", err)
@@ -16,14 +18,17 @@ func main() {
 	defer connection.Close()
 
 	client := protobuffer.NewHelloServiceClient(connection)
-
-	request := &protobuffer.HelloRequest{
-		Name: "Bruno",
-	}
-
-	res, err := client.Hello(context.Background(), request)
+	res, err := hello(client, arguments[0])
 	if err != nil {
-		log.Fatalf("Error during execution:\n %v", err)
+		log.Fatalf("Error during procedure call:\n%v", err)
 	}
 	log.Println(res)
+}
+
+func hello(client protobuffer.HelloServiceClient, name string) (*protobuffer.HelloResponse, error) {
+	request := &protobuffer.HelloRequest{
+		Name: name,
+	}
+	res, err := client.Hello(context.Background(), request)
+	return res, err
 }
